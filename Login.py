f96b2c10f35c9bb6aa627d0cd40927eb234308f7
K7Unk <ExtacyLV@icloud.com>
2025-12-04 11:56:45 +0700

    Branch Baru

--- Login.py
+++ Login.py
@@ -1,6 +1,12 @@
 user = []
+admin = [
+    {
+        'username' : 'adminveggiet',
+        'password' : 'sehat321'
+    }
+]
 homepage = True
-
+adminmenu = True
 def cekuser(x):
     for i in user:
         if i['username'].lower() != x:
@@ -20,51 +26,70 @@ def login(x,y):
             return True
     return False
             
-    
+def adminlogin(x,y):
+    for i in admin:
+        if i['username'].lower() == "adminveggiet" and i['password'] == 'sehat321':
+             return menuadmin()
+    return False
 # masuk=True
-while True:
+def menulogin():
+    while True:
 
-    print("=== LOGIN/SIGN UP ==")
-    print("1. Sign Up")
-    print("2. Login")
-    print("3. Quit")
-    input1 = input("Pilih Opsi:")
-    
-    if input1 == "1":
-        print("\n=== SIGN UP ===")
-        while True:
-            SUname = input("Masukan Username:")
-            SUpass = input("Masukan Password:")
-            
-            if cekuser(SUname):
-                print("Username Telah Dipakai!")
-            else:
-                signup(SUname,SUpass)
-                print("Selamat Akun Anda Berhasil Dibuat!")
-                break
-            
-            
-            
+        print("=== LOGIN/SIGN UP ==")
+        print("1. Sign Up")
+        print("2. Login")
+        print("3. Quit")
+        input1 = input("Pilih Opsi:")
         
+        if input1 == "1":
+            singup()
+        elif input1 == "2":
+            loginmenu()
+        elif input1 == "3":
+            break
+
+def singup():
+    print("\n=== SIGN UP ===")
+    while True:
+        SUname = input("Masukan Username:")
+        SUpass = input("Masukan Password:")
         
-    elif input1 == "2":
-        print("\n=== LOGIN ===")
-        while True:
-            nameinput = input("Username:")
-            passinput = input("Password:")
+        if cekuser(SUname):
+            print("Username Telah Dipakai!")
+        else:
+            signup(SUname,SUpass)
+            print("Selamat Akun Anda Berhasil Dibuat!")
+            break      
+                
+            
+def loginmenu():
+    print("\n=== LOGIN ===")
+    while True:
+        nameinput = input("Username:")
+        passinput = input("Password:")
+        if adminlogin(nameinput,passinput) != False:
             if login(nameinput, passinput):
                 print("Selamat Anda Berhasil Login")
-                homepage = False
-                break
-            else:
-                print("Password Atau Username Salah!")
-    
-    elif input1 == "3":
+                menu()
+            menuadmin()
+            break
+            
+            menu()
+            break
+        else:
+            print("Password Atau Username Salah!")
+
+def menu(nameinput):
+    while True:
+        print(f"Halo {nameinput}")
         break
     
-if not homepage:
+
+def menuadmin():
     while True:
-        pass
-    
-else:
-    print("See You Next Time :)")
+        print("LOop")
+        break
+
+menulogin()
+
+print("See You Next Time :)")
