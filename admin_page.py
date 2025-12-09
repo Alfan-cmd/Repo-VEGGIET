76567b0bc8d25fa13ed5c4ffe3498a3fd9387e7e
zalisaneri <irenasilaz17@student.upi.edu>
2025-12-09 11:58:22 +0700

    menambah data_veggiet.csv

---
+++ admin_page.py
@@ -0,0 +1,13 @@
+import pandas as pd
+df= pd.read_csv("daftar_veggiet.csv")
+
+def tambah_barang():
+    jumlah = int(input("Masukkan Jumlah Jenis Barang: "))
+    for i in range(1, jumlah + 1):
+        print(f"--- data barang ke {i} ---")
+        produk = input("Nama: ")
+        kode = int(input("Kode makanan : "))
+        stok = int(input("Jumlah Stok: "))
+        
+        df.loc[len(df)] = [kode, produk, stok]
+print("Hasil: \n", df.tail())
