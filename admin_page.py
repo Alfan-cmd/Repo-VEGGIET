3b464ada5345d4722b4320637323418645347ac5
zalisaneri <irenasilaz17@student.upi.edu>
2025-12-12 20:12:18 +0700

    memperbaiki Input Admin

--- admin_page.py
+++ admin_page.py
@@ -2,14 +2,16 @@ import pandas as pd
 df= pd.read_csv("daftar_veggiet.csv")
 
 def tambah_barang():
-    jumlah = int(input("Masukkan Jumlah Jenis Barang: "))
+    jumlah = int(input("Masukkan Jumlah data yang ingin dimasukkan: "))
     for i in range(1, jumlah + 1):
-        print(f"--- data barang ke {i} ---")
+        print(f"--- daata Veggiet ke {i} ---")
         produk = input("Nama: ")
         kode = input("Kode makanan : ")
         stok = int(input("Jumlah Stok: "))
+        harga = int(input("Harga : "))
+        kategori = input("Kategori : ")
+
         
-        df.loc[len(df)] = [kode, produk, stok]
+        df.loc[len(df)] = [kode, produk, stok, harga, kategori] #untuk memasukkan data
     print(f"Hasil: \n {df}")
 tambah_barang()
-#halooo
